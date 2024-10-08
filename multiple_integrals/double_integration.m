42365e1569f2985501b106ece3512c0d969f76bd
Abhyuday Vaish <63145440+Abhyuday-06@users.noreply.github.com>
2024-10-08 16:42:42 +0530

    Update double_integration.m

--- multiple_integrals/double_integration.m
+++ multiple_integrals/double_integration.m
@@ -1,3 +1,5 @@
+% Visualising the volume of a sphere %
+
 clc 
 clear all
 syms x y z
@@ -6,9 +8,19 @@ viewSolid(z, 0+0*x*y, sqrt(1-x^2-y^2), y, 0 + 0*x, sqrt(1-x^2), x, 0, 1);
 axis equal;
 grid on;
 
+% Visualising the volume of a paraboloid z = x^2 + y^2 bounded by y = 2*x and y = x^2%
+
 clc 
 clear all
 syms x y z
 vol = int(int(x^2 + y^2, x, y/2, sqrt(y)), y, 0, 4)
 viewSolidone(z, 0+0*x*y, x^2 + y^2, x, y/2, sqrt(y), y, 0, 4);
 grid on;
+
+% Visualising the volume using cyiindrical coordinates %
+
+clc
+clear all
+syms r theta
+vol = int(int(1-r^2)*r, r, 0,1), theta, 0, 2*pi)
+fsurf(r*cos(theta), r*sin(theta 0                      
