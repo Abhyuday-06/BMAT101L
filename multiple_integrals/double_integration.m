84b8b1cdff55b079ed1bf948d57cf7b6ffcd5484
Abhyuday Vaish <63145440+Abhyuday-06@users.noreply.github.com>
2024-10-08 17:21:32 +0530

    Update double_integration.m

--- multiple_integrals/double_integration.m
+++ multiple_integrals/double_integration.m
@@ -28,3 +28,33 @@ axis equal;
 axis([-2 2 -2 2 - 1.3])
 xticks(-2:2); yticks(-2:2); zticks(0:1.3);
 xlabel('x'); ylabel('y')
+
+% Evaluating double integral ysin(xy)dxdy over R = [1,2]x[0,pi] %
+
+clc 
+clear all
+syms x y z
+viewSolidone(z,0+0*x+0*y, y*sin(x*y), x, 1+0*y, 2+0*y, y, 0, pi)
+v = int(int(y*sin(x*y),x,1,2),y,0,pi) % for printing the numerical value %
+
+% Evaluate double integral x-2y^2 dydx where R = {(x,y) | 0 <= x <= 2, | 1 <= y <= 2}
+
+clc
+clear al
+syms x y z
+viewSolid(z, 0+0*x+0*y, x-3*y&2+0*y, y,1+0*x, 2+0*y, x, 0, 2)
+v = int(int(x-3*y^2+ 0*y,y,1,2),x,0,2)
+
+% Find the volume of the solid that lies under the paraboloid z = x^2 + y^2 and above the xy plane and niside the cylinder x^2 + y^2 = 2*x %
+
+clc 
+clear all
+syms r theta z r1
+V = int(int((r^2)*r, r, 0, 2*cos(theta)), theta, -pi/2, pi/2)
+r = 2*cos(theta), x = r*cos(theta), y = r*sin(theta)
+fsurf(x,y,z, [0 2*pi 0 1], 'MeshDensity', 16) % For Cartesian coordinates %
+axis equal; 
+xlabel('x'); ylabel('y'); zlabel('z');
+zticks(0:1.5)
+hold on
+fsurf(r1*cos(theta), r1*sin(theta), r1^2, [0 1 0 2*pi], 'MeshDensity', 20) % For Polar coordinates %
